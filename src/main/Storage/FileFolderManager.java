285bcf9d2b5d2b9fe54e464f93c6d0b9277b35ca
Eddiec2084 <eddiec2084@gmail.com>
2026-04-30 01:02:18 -0400

    Fix syntax errors and improve FileFolderManager methods
    Refactor FileFolderManager methods to correct syntax errors and improve functionality.

--- src/main/Storage/FileFolderManager.java
+++ src/main/Storage/FileFolderManager.java
@@ -13,38 +13,41 @@ public class FileFolderManager {
      * @param path file path
      * @throws IOException if file exist or path not valid
      */
-    public static void createFile(String path) throws IOException {}
+    public static void createFile(String path) throws IOException {
         File file = new file(path);
         file.createNewFile();
-    
-    public static void deleteFile(String path) throws IOException{}
+    }
+    public static void deleteFile(String path) throws IOException{
         File file = new file(path);
         file.delete();
-    
-    public static boolean isFileExists(String path) throws IOException{}
+    }
+    public static boolean isFileExists(String path) throws IOException{
         File file = new File();
         return file.exists() && file.isFile();
+    }
     
-    public static String[] listFilesInFolder(String path) throws IOException{}
+    public static String[] listFilesInFolder(String path) throws IOException{
         File folder = new File(path);
         String[] Contents = folder.list();
         return Contents;
-
-    public static String readFile(String path) throws IOException{}
-        return String(Files.readallBytes(Paths.get(path)));l
-
-    public static void writeFile(String path, String content) throws IOException{}
-        Files.write(Paths.get(path), content.getBytes())
-            
-    public static void createFolder(String path) throws IOException{}
+    }
+    public static String readFile(String path) throws IOException{
+        return String(Files.readallBytes(Paths.get(path)));
+    }
+    
+    public static void writeFile(String path, String content) throws IOException{
+        Files.write(Paths.get(path), content.getBytes());
+    }
+    
+    public static void createFolder(String path) throws IOException{
         File folder = new File(path);
         folder.mldir();
-
-    public static boolean isFolderExists(String path) {}
+    }
+    public static boolean isFolderExists(String path) {
         File folder = new File(path);
         return file.exists() && file.isDirectory();
-
-    public static String[] listFoldersInFolder(String path) {}
+    }
+    public static String[] listFoldersInFolder(String path) {
         File folder = new File(path);
         String[] Contents = folder.list();
         
@@ -55,9 +58,9 @@ public class FileFolderManager {
                 folders[i++] = f.getname();
             }    
         }   
-
-    public static void deleteFolder(String path) throws IOException{}
+    }
+    public static void deleteFolder(String path) throws IOException{
         File file = new file(path);
         file.delete();
-
+    }
 }
